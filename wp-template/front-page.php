3c0d6fc0d15e5feef2bb319e318ff7038d1d494d
Kiran Nambiar <kiran.nambiar@gmail.com>
2014-09-14 16:27:50 -0400

    custom front page
    includes refactor to have a more standard index.php page

---
+++ wp-template/front-page.php
@@ -0,0 +1,48 @@
+<?php 
+	get_header();
+	$philosophy_page = get_page_by_path('philosophy');
+	$products_page = get_page_by_path('products');
+	$events_page = get_page_by_path('events');
+	$stores_page = get_page_by_path('stores');
+	$press_page = get_page_by_path('press-links');
+?>
+
+<div id="main">
+	<div id="content">
+		
+		<div class="main-content main-section">
+			<h1 class="page-title philosophy"><a name="philosophy" class="page-title"><?php echo $philosophy_page->post_title; ?></a></h1>
+			<?php echo $philosophy_page->post_content; ?>
+		</div>
+
+		<div class="products-content main-section">
+			<h1 class="page-title right-heading"><a name="products" class="page-title"><?php echo $products_page->post_title; ?></a></h1>
+			<?php echo $products_page->post_content; ?>
+		</div>
+
+		<div class="events-content main-section">
+			<div><h1 class="page-title left-heading"><a name="events" class="page-title"><?php echo $events_page->post_title; ?></a></h1></div>
+			<div class="main-sub-section"><?php echo do_shortcode('[events_list scope="future" limit=5 pagination=1]'); ?></div>
+		</div>
+
+		<div class="store-loc-content main-section">
+			<h1 class="page-title left-heading"><a name="stores" class="page-title"><?php echo $stores_page->post_title; ?></a></h1>
+			<?php echo $stores_page->post_content; ?>
+		</div>
+
+		<div class="newsletter main-section">
+			<?php echo do_shortcode("[sp-signup email_label='SUBSCRIBE TO THE VAYA NEWSLETTER' listids='109' redirect_page='' lists_checked='1' label_display='false' thank_you='Thank you for subscribing!' button_text='Submit' ]"); ?>
+		</div>
+		
+		<div class="press-content main-section">
+			<h1 class="page-title left-heading"><a name="press" class="page-title"><?php echo $press_page->post_title; ?></a></h1>
+			<?php echo $press_page->post_content; ?>
+		</div>
+
+	</div>
+	<div> this is using the front-page.php file </div>
+</div>
+
+<?php get_footer(); ?>
+
+
--- wp-template/index.php
+++ wp-template/index.php
@@ -1,47 +1,18 @@
 <?php 
 	get_header();
-	$philosophy_page = get_page_by_path('philosophy');
-	$products_page = get_page_by_path('products');
-	$events_page = get_page_by_path('events');
-	$stores_page = get_page_by_path('stores');
-	$press_page = get_page_by_path('press-links');
 ?>
 
 <div id="main">
 	<div id="content">
-		
-		<div class="main-content main-section">
-			<h1 class="page-title philosophy"><a name="philosophy" class="page-title"><?php echo $philosophy_page->post_title; ?></a></h1>
-			<?php echo $philosophy_page->post_content; ?>
-		</div>
-
-		<div class="products-content main-section">
-			<h1 class="page-title right-heading"><a name="products" class="page-title"><?php echo $products_page->post_title; ?></a></h1>
-			<?php echo $products_page->post_content; ?>
-		</div>
-
-		<div class="events-content main-section">
-			<div><h1 class="page-title left-heading"><a name="events" class="page-title"><?php echo $events_page->post_title; ?></a></h1></div>
-			<div class="main-sub-section"><?php echo do_shortcode('[events_list scope="future" limit=5 pagination=1]'); ?></div>
-		</div>
-
-		<div class="store-loc-content main-section">
-			<h1 class="page-title left-heading"><a name="stores" class="page-title"><?php echo $stores_page->post_title; ?></a></h1>
-			<?php echo $stores_page->post_content; ?>
-		</div>
-
-		<div class="newsletter main-section">
-			<?php echo do_shortcode("[sp-signup email_label='SUBSCRIBE TO THE VAYA NEWSLETTER' listids='109' redirect_page='' lists_checked='1' label_display='false' thank_you='Thank you for subscribing!' button_text='Submit' ]"); ?>
-		</div>
-		
-		<div class="press-content main-section">
-			<h1 class="page-title left-heading"><a name="press" class="page-title"><?php echo $press_page->post_title; ?></a></h1>
-			<?php echo $press_page->post_content; ?>
-		</div>
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			<h2><?php the_title(); ?></h2>
+			<p><?php the_content() ?></p>
+		<?php endwhile; else: ?>
+			<p><?php _e('Sorry, there are no posts to display'); ?></p>
+		<?php endif; ?>
 
 	</div>
 </div>
-
 <?php get_footer(); ?>
 
 
