4e4870bee300d2cfcea85f082ac0216e250901bf
Kiran Nambiar <kiran.nambiar@gmail.com>
2014-09-14 16:33:10 -0400

    added wp bundle to aptana

--- wp-template/index.php
+++ wp-template/index.php
@@ -6,7 +6,7 @@
 	<div id="content">
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 			<h2><?php the_title(); ?></h2>
-			<p><?php the_content() ?></p>
+			<p><?php the_content(); ?></p>
 		<?php endwhile; else: ?>
 			<p><?php _e('Sorry, there are no posts to display'); ?></p>
 		<?php endif; ?>
